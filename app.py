f8ab335b91ea212cf088c2752d542d734573fcac
Matheus Emanoel Rodrigues De Albuquerque <lab.emanoel@gmail.com>
2025-05-28 08:51:12 -0300

    UPDATED: app.py - initialize JWT with routes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 from database import db
+from routes.AuthRoute import auth_bp
 from routes.SalaRoute import sala_bp
 from routes.ReservaRoute import reserva_bp
 from routes.HomeRoute import home_bp
@@ -11,9 +13,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 
 db.init_app(app)
 
+jwt = JWTManager(app)
+
 with app.app_context():
     db.create_all()
 
+app.register_blueprint(auth_bp)
 app.register_blueprint(sala_bp)
 app.register_blueprint(reserva_bp)
 app.register_blueprint(home_bp)
