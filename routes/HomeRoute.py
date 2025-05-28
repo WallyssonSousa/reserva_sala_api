d4acf2a4906d5eafc313807eab9da06be0cd0bc9
Matheus Emanoel Rodrigues De Albuquerque <lab.emanoel@gmail.com>
2025-05-28 11:57:25 -0300

    UPDATED: removed required auth from HomeRoute

--- routes/HomeRoute.py
+++ routes/HomeRoute.py
@@ -4,6 +4,5 @@ from flask_jwt_extended import jwt_required
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/', methods=['GET'])
-@jwt_required()
 def home():
     return jsonify({"message": "Bem-vindo ao sistema de reservas de salas!"}), 200
