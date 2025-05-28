08a5f35c2d7f500d5df34fdc83ebd8077bfce728
Matheus Emanoel Rodrigues De Albuquerque <lab.emanoel@gmail.com>
2025-05-28 08:50:39 -0300

    UPDATED: config.py - App Port, JWT settings, database connection

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
+from datetime import timedelta
 import os 
 
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def configure_app(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
+    app.config["JWT_TOKEN_LOCATION"] = ["headers"]
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['HOST'] = '0.0.0.0'
-    app.config['PORT'] = 5000
+    app.config['PORT'] = 5001
     app.config['DEBUG'] = True
+    print("Banco de dados:", app.config['SQLALCHEMY_DATABASE_URI'])
